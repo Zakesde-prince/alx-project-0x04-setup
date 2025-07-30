95a5b59fa396341435ab8d94a4f16df0d49463a1
Zakesde-prince <prince.feela2905@gmail.com>
2025-07-30 20:56:40 +0200

    .

---
+++ alx-project-0x06/components/common/Button.tsx
@@ -0,0 +1,16 @@
+import { ButtonProps } from "@/interface";
+
+export default function Button({
+  action,
+  buttonLabel,
+  buttonBackgroundColor,
+}: ButtonProps) {
+  return (
+    <button
+      onClick={action}
+      className={`px-4 py-2 rounded-md text-white bg-${buttonBackgroundColor}-500 hover:bg-${buttonBackgroundColor}-600 transition`}
+    >
+      {buttonLabel}
+    </button>
+  );
+}
